9b54d193a449fef74e19721053a42b6712830998
Sk-max <1078527213@qq.com>
2021-02-06 22:13:13 +0800

    im a test1

---
+++ usian-content-feign/src/main/java/com/usian/test1/Study.java
@@ -0,0 +1,16 @@
+package com.usian.test1;
+
+public class Study {
+
+    public static void main(String[] args) {
+
+        System.out.println("im a test1");
+        System.out.println("im a test1");
+        System.out.println("im a test1");
+        System.out.println("im a test1");
+
+
+    }
+
+
+}
