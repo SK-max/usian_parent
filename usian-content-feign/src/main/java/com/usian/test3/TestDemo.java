a7c67c17a7e7608c22f8508f47f43dfa5f66ad7c
Sk-max <1078527213@qq.com>
2021-02-06 22:58:43 +0800

    im a test1

--- usian-content-feign/src/main/java/com/usian/test3/TestDemo.java
+++ usian-content-feign/src/main/java/com/usian/test3/TestDemo.java
@@ -5,6 +5,7 @@ public class TestDemo {
     public static void main(String[] args) {
         for (int i=1;i<100;i++){
             System.out.println("test!");
+            System.out.println("haha ");
         }
 
     }
